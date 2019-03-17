6f5cdd7a4058bad8affdf6cd945ea0895a113125
Kaveh Mousavi Zamani <kaveh@fitanalytics.com>
2019-03-17 15:03:41 +0100

    fix lint issues in httpclient/httpclient_test.go

--- httpclient/httpclient_test.go
+++ httpclient/httpclient_test.go
@@ -7,19 +7,22 @@ import (
 )
 
 type jsonTest struct {
-	Field string `json:field`
-	Value int64  `json:value`
+	Field string `json:"field"`
+	Value int64  `json:"value"`
 }
 
 func TestJSON(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(`
+		_, err := w.Write([]byte(`
 		{
 			"field": "test",
 			"value": 100
 		}
 		`))
+		if err != nil {
+			t.Error("Test server failed")
+		}
 	}))
 	defer server.Close()
 
